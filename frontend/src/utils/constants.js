330fb0a40740d0dcd79cb5be448a6267a6149e0b
Oussema Benslimene <149532574+OussemaBenslimene@users.noreply.github.com>
2024-12-21 01:34:35 +0100

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const API_ROOT = process.env.BUILD_MODE === 'dev'
-  ? "/api/" + import.meta.env.VITE_DEV_API_ROOT
-  : "/api/" + import.meta.env.VITE_PROD_API_ROOT
+  ? import.meta.env.VITE_DEV_API_ROOT
+  : import.meta.env.VITE_PROD_API_ROOT
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
